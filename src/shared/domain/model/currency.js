e0f501248f8a7de1f1a726772d21079b9c599c50
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-13 17:57:39 -0500

    refactor(currency): replace error throwing with validation error for invalid currency codes.

--- src/shared/domain/model/currency.js
+++ src/shared/domain/model/currency.js
@@ -1,10 +1,12 @@
+import {ValidationError} from "./errors.js";
+
 class Currency {
     static #VALID_CODES = ['USD', 'EUR', 'GBP', 'JPY'];
     #code;
 
     constructor(code) {
         if (!Currency.#VALID_CODES.includes(code))
-            throw new Error(`Invalid code: ${code}. Must be one of ${Currency.#VALID_CODES.join(', ')}`);
+            throw new ValidationError(`Invalid code: ${code}. Must be one of ${Currency.#VALID_CODES.join(', ')}`);
         this.#code = code;
     }
 }
